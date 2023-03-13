5670eab80b5bce4656b15bd884d9194661142c57
Nicholas Gabriel <109010700+Inicbrz@users.noreply.github.com>
2023-03-12 23:18:54 -0300

    e1
    edit 1

---
+++ script.js
@@ -0,0 +1,19 @@
+const sobreabrir = document.querySelector('.botaosobreabrir');
+const secaosobre = document.querySelector('.sobresection');
+const sobrefechar = document.querySelector('.botaosobrefechar');
+const cliqueaqui = document.querySelector('.clickhere');
+const clicouaparecemiguel = document.querySelector('.peaky');
+
+
+// sobreabrir.addEventListener('click', ()=> {
+//     secaosobre.classList.add('active');
+// });
+
+// sobrefechar.addEventListener('click', ()=> {
+//     secaosobre.classList.remove('active');
+// });
+
+cliqueaqui.addEventListener('click', ()=> {
+    clicouaparecemiguel.classList.add('active');
+});
+
